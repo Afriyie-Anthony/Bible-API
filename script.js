734a43e76c8fa451a91368ff99e6b4fa3000dbc8
Afriyie-Anthony <kobbydalton@icloud.com>
2024-10-16 15:38:00 +0000

    initial

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ inputVerse.addEventListener('input', () => {
   debounceTimeout = setTimeout(() => {
     requestUserInput(verse);
   }, 1000);
-  // Loader.style.display = "block"
+   Loader.style.display = "block"
 })
 
 //Set preloader to none
